88ce1e90eb333707c7512c270db89706feff9993
Estllanav03 <estllanav@alu.edu.gva.es>
2023-11-17 19:45:56 +0100

    comit + push

--- PracticaGitHub/PracticaGitHub/Form1.Designer.cs
+++ PracticaGitHub/PracticaGitHub/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.btnCalcular.TabIndex = 11;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // lblCoste
             // 
