88876d11e9f3883bef996f075bac0a53d6855826
Estllanv03 <roseblinks1415@gmail.com>
2023-11-17 20:10:16 +0100

    commit + push

--- PracticaGitHub/PracticaGitHub/Form1.cs
+++ PracticaGitHub/PracticaGitHub/Form1.cs
@@ -32,13 +32,13 @@ namespace PracticaGitHub
                 if (cbUrgente.Checked)
                     tipoTelegrama = 'u';
                 //Obtengo el número de palabras que forma el telegrama
-                numPalabras = textoTelegrama.Length;
+                numPalabras = textoTelegrama.Split(' ').Length;
                 //Si el telegrama es ordinario
                 if (tipoTelegrama == 'o')
                     if (numPalabras <= 10)
-                        coste = 25;
+                        coste = 2.5;
                     else
-                        coste = 0.5 * numPalabras;
+                        coste = 2.5 * (numPalabras - 10);
                 else
                 //Si el telegrama es urgente
                 if (tipoTelegrama == 'u')
